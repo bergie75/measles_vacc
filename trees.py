fe88d525ec8cfdc8243b23b2025cf6bbaaf65fb3
eb <89044765+bergie75@users.noreply.github.com>
2025-10-09 14:58:02 -0400

    Started building representation for decision agents

---
+++ trees.py
@@ -0,0 +1,84 @@
+import numpy as np
+import random
+
+max_simulation_depth = 180
+max_pop = 5000
+action_set = ["increase_vax_rate", "apply_npi", "diagnostic_measurement", "wes_measurement"]
+input_threshold_ranges = {"time_since_diag": [0, max_simulation_depth],
+                    "time_since_wes": [0, max_simulation_depth],
+                      "current_diag": [0, max_pop],
+                        "current_wes": [0, max_pop]}
+
+class Node:
+    def __init__(self, action=None, decision_var=None, threshold=None, left_child=None, right_child=None):
+        self.action = action
+        self.decision_var = decision_var
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+
+    def __repr__(self):
+        if self.action is not None:
+            return self.action
+        elif (self.threshold is not None) and (self.decision_var is not None):
+            return f"{self.decision_var} >= {self.threshold}"
+        else:
+            return "ERROR: malformed node"
+
+    def terminal_leaf(self):
+        return (self.action is not None)
+    
+    # defaults to taking a new value between 80% and 120% of old value
+    # enforces threshold limits
+    def mutate_threshold(self, max_perc_change=0.2):
+        rng = np.random.default_rng()
+        multiplier = rng.uniform(low=1-max_perc_change, high=1+max_perc_change)
+        candidate_value = self.threshold*multiplier
+
+        if input_threshold_ranges[self.decision_var][0] <= candidate_value <= input_threshold_ranges[self.decision_var][0]:
+            self.threshold = candidate_value
+    
+    # inputs will be in the form of a dictionary {str(name_decision_var): float(value_of_the_variable)}
+    def evaluate(self, inputs):
+        # terminal condition to end the recursion
+        if self.terminal_leaf():
+            return self.action
+        
+        # check threshold to determine the branch to follow, uses dictionary structure of inputs
+        if inputs[self.decision_var] <= self.threshold:
+            return self.left_child.evaluate(inputs)
+        else:
+            return self.right_child.evaluate(inputs)
+
+
+class DecisionTree:
+    def __init__(self, root_node=None):
+        self.root_node = root_node
+
+    def evaluate(self, inputs):
+        return self.root_node.evaluate(inputs)
+    
+# used for mutation if node is currently a terminal leaf
+def generate_new_decision_node():
+    rng = np.random.default_rng()
+    decision_var, range = random.choice(list(input_threshold_ranges.items()))
+    threshold = rng.uniform(low=range[0], high=range[1])
+
+    left_child = Node(action=random.choice(action_set))
+    right_child = Node(action=random.choice(action_set))
+
+    return Node(decision_var=decision_var, threshold=threshold, left_child=left_child, right_child=right_child)
+
+# a helper to avoid cloning by reference when mating trees
+def copy_node(target_node):
+    # base case for recursion
+    if target_node.action is not None:
+        return Node(target_node.action)
+    else:
+        decision_var = target_node.decision_var
+        threshold = target_node.threshold
+        left_child = copy_node(target_node.left_child)
+        right_child = copy_node(target_node.right_child)
+        
+        return Node(decision_var=decision_var, threshold=threshold,
+                    left_child=left_child, right_child=right_child)
