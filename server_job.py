2892b629dde4017423318e9406899e164a15b456
eb <89044765+bergie75@users.noreply.github.com>
2025-11-04 16:13:59 -0500

    fixed small bug

--- server_job.py
+++ server_job.py
@@ -5,5 +5,5 @@ import os
 #warnings.filterwarnings("ignore")
 #thresholds = check_basic_reproduction_number(disease_params)
 #print(f"Threshold 1: {thresholds[0]}, Threshold 2: {thresholds[1]}")
-reload = os.path.join("custom_starting_ensembles", "first_handcrafted", "round_0")
+reload = os.path.join("custom_starting_ensembles", "first_handcrafted")
 optimize(number_of_members, max_rounds, 2, reload=reload, run_name="serverpytest")
